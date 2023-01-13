960e1f71c5343c727dcb9c8e1a4603d6016bd40f
sohel0199 <sohel019954@gmail.com>
2023-01-14 02:08:23 +0600

    first commit

---
+++ src/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            This is home page
+            bal kaj kore nah kend
+        </div>
+    );
+};
+
+export default Home;
